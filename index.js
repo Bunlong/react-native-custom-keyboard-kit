1a075af448b0c9832d457ebc236abb0f8716408a
Bunlong <bunlong.van@gmail.com>
2018-10-16 16:44:07 +0700

    Xxport native methods for calling in React Native javascript

--- index.js
+++ index.js
@@ -1,6 +1,74 @@
 
 import { NativeModules } from 'react-native';
 
-const { RNCustomKeyboardKit } = NativeModules;
+const { CustomKeyboardKit } = NativeModules;
 
-export default RNCustomKeyboardKit;
+import {
+  NativeModules,
+  TextInput,
+  findNodeHandle,
+  AppRegistry,
+} from 'react-native';
+
+const {
+  install, uninstall,
+  insertText, backSpace, doDelete,
+  moveLeft, moveRight,
+  switchSystemKeyboard,
+} = CustomKeyboardKit;
+
+export {
+  install, uninstall,
+  insertText, backSpace, doDelete,
+  moveLeft, moveRight,
+  switchSystemKeyboard,
+};
+
+const keyboardTypeRegistry = {};
+
+export function register(type, factory) {
+  keyboardTypeRegistry[type] = factory;
+}
+
+class CustomKeyboardKitContainer extends Component {
+  render() {
+    const {tag, type} = this.props;
+    const factory = keyboardTypeRegistry[type];
+    if (!factory) {
+      console.warn(`Custom keyboard type ${type} not registered.`);
+      return null;
+    }
+    const Comp = factory();
+    return <Comp tag={tag} />;
+  }
+}
+
+AppRegistry.registerComponent("CustomKeyboard", () => CustomKeyboardKitContainer);
+
+export class CustomTextInput extends Component {
+  static propTypes = {
+    ...TextInput.propTypes,
+    customKeyboardType: PropTypes.string,
+  };
+
+  componentDidMount() {
+    install(findNodeHandle(this.input), this.props.customKeyboardType);
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.customKeyboardType !== this.props.customKeyboardType) {
+      install(findNodeHandle(this.input), newProps.customKeyboardType);
+    }
+  }
+
+  onRef = ref => {
+    this.input = ref;
+  };
+  
+  render() {
+    const { customKeyboardType, ...others } = this.props;
+    return <TextInput {...others} ref={this.onRef}/>;
+  }
+}
+
+// export default CustomKeyboardKit;
